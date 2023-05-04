a28da4467263332ed75851f1b2c259a3cdf32338
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-05-04 23:19:43 +0900

    create model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.department_name
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=100)
+    join_year = models.IntegerField()
+    department = models.ForeignKey(Department,related_name='employees', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.employee_name
